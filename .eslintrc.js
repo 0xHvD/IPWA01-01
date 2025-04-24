c290eafa5554e70e5e1c9498653e8739bf2811eb
Henri van Dorsten <henrivandorsten@googlemail.com>
2025-04-24 22:13:29 +0200

    Added lint-script for CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
     extends: [
       'plugin:vue/vue3-standard',
-      'plugin:prettier/recommended'
-    ]
+      'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
+    rules: {
+    },
   }
+  
